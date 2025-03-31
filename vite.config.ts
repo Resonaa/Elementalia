166d1ee060dd7a4f6516bc478a0a28c0b2c63941
Resonaa <resonaa@outlook.com>
2025-03-31 18:41:20 +0800

    feat: PWA

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "围猫高爆服 ～ Éclipse Containment",
+        short_name: "围猫高爆服",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "fullscreen",
+        background_color: "#FFFFFF",
+        theme_color: "#b3d9ff",
+        description: "散人打金超高爆率，是兄弟就来围我！",
+        lang: "zh-CN",
+      },
+    }),
+  ],
+});
